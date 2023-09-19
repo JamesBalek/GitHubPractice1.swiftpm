0897538cf93f0dcfde98592d5793cab2faa31bac
Teodor Djuric <tdjuric5081@stu.d214.org>
2023-09-19 10:48:29 -0500

    Created a list for all chores

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tasks = ["Mow the Lawn", "Feed the Dog", "Grocery Shop"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List{
+            ForEach(0..<tasks.count, id: \.self) { index in
+                Text(tasks[index])
+            }
         }
     }
 }
