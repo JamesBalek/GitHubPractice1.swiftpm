4d7a52fe1ac2b53d4786d1ebf099c3693f204e56
Teodor Djuric <tdjuric5081@stu.d214.org>
2023-09-21 10:22:25 -0500

    Converted tasks from string to task object

--- ContentView.swift
+++ ContentView.swift
@@ -2,12 +2,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var tasks = ["Mow the Lawn", "Feed the Dog", "Grocery Shop"]
+    @State var tasks: [Task] = [Task(nameOfTask: String())]
     
     var body: some View {
         List{
             ForEach(0..<tasks.count, id: \.self) { index in
-                Text(tasks[index])
+                Text(tasks[index].nameOfTask)
             }
         }
     }
